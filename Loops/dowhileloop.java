eb4b0286660aac41266d3d2901e1695a91416612
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-25 14:51:28 +0800

    added dowhile loop practice

---
+++ Loops/dowhileloop.java
@@ -0,0 +1,10 @@
+public class dowhileloop {
+    public static void main(String[] argv) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+
+    }
+}
