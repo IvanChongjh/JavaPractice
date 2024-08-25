881cfb5f8c44758ef9918e5105f0845dbb6f5201
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-25 17:30:47 +0800

    Added Classes Practice

---
+++ Classes/CreatingClasses.java
@@ -0,0 +1,20 @@
+package Classes;
+
+public class CreatingClasses {
+    String name = "Default";
+    int age = 0;
+
+
+    public static void main(String[] args)
+    {   
+
+        CreatingClasses test = new CreatingClasses();
+        System.out.println(test.name);
+        System.out.println(test.age);
+
+        test.age = 50;
+
+        System.out.println(test.age);
+
+    }
+}
