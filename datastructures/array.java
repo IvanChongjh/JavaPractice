b48aeba8111dd22c78cf4626cb912b59716c2c90
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-26 13:27:06 +0800

    added array practice

---
+++ datastructures/array.java
@@ -0,0 +1,15 @@
+package datastructures;
+
+public class array {
+    public static void main(String[] args) {
+
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+        for (int i = 0 ; i < 4; ++i)
+        {
+            System.out.println(cars[i]);
+        }
+
+    }
+
+}
