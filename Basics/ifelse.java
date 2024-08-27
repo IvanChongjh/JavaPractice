2851b5d60e90181139200f33f645f268c383f158
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-27 20:35:35 +0800

    Added ifelse statement practice

---
+++ Basics/ifelse.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class ifelse {
+
+    public static void main(String[] args) {
+        int testscore = 10;
+
+        if (testscore >= 50) {
+            System.out.println("You are above 0 ");
+        } else
+            System.out.println("You have passed the test");
+        
+    }
+}
