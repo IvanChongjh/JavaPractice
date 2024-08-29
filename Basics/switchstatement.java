56102f25db1ffd0aed07e0a9a0ec4f643fa0903d
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-29 19:20:21 +0800

    Added switch statements practice

---
+++ Basics/switchstatement.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class switchstatement {
+
+    public static void main(String[] args)
+    {
+        int userChoice = 2;
+
+
+        switch(userChoice)
+        {
+            case 1: System.out.println("Choice 1 selected");
+            break;
+
+            case 2: System.out.println("Choice 2 selected");
+            break;
+        }
+    }
+}
