cc6126d596be47dca727ce744f1f7edf87882c1c
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-31 22:21:22 +0800

    Added date feature practice

---
+++ Basics/date.java
@@ -0,0 +1,9 @@
+package Basics;
+import java.time.LocalDate; // import for local date library
+public class date {
+    public static void main(String [] args)
+    {   
+        LocalDate date = LocalDate.now();       
+        System.out.println(date);
+    }
+}
