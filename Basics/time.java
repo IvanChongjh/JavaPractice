64587499dfeb4215fd3372fc39a39e9c30ec74a7
IvanCHongjh <ivancjh.six@gmail.com>
2024-08-31 22:25:57 +0800

    added time library practice

---
+++ Basics/time.java
@@ -0,0 +1,12 @@
+package Basics;
+
+import java.time.LocalTime;
+
+public class time {
+    public static void main(String[] args)
+    {
+        LocalTime timenow = LocalTime.now();
+        System.out.println(timenow);
+
+    }
+}
